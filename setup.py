98f6cd18915d966ed52a4e200c5956c6a20cfb4d
sepidekoohfar <sepideh.koohfar@unh.edu>
2023-12-31 15:01:32 -0600

    updating git

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='forecastblurdenoise',
-    version='0.1.0',
+    version='0.1.1',
     packages=['forecastblurdenoise'],
     install_requires=[
         # List your dependencies here
