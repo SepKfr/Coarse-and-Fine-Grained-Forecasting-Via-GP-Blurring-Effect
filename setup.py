47fbd1a18a52c3f3d4e3868e80239dd1fff6a508
sepidekoohfar <sepideh.koohfar@unh.edu>
2024-01-02 12:44:52 -0600

    Updating meta and setup files for conda and PyPi distribution

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from setuptools import setup
-
+from forecastblurdenoise import __version__
 setup(
     name='forecastblurdenoise',
-    version='0.1.1',
+    version=__version__,
     packages=['forecastblurdenoise'],
     install_requires=[
-        # List your dependencies here
-        'torch',
-        'numpy',
-        'pandas',
-        'optuna',
-        'gpytorch'
-        # Add other dependencies as needed
+        'python>=3.10',
+        'gpytorch>=1.9.0',
+        'torch>=2.0.1',
+        'pandas>=1.5.2',
+        'numpy>=1.23.5',
+        'optuna>=3.3.0'
     ],
     license='MIT'
 )
